3a352360343fbcc9b9657f503c57a56e4c1e1fc4
Christopher Burge and Jacqueline Remmel <christopher@>
2019-10-02 11:51:04 -0700

    refactor code.

--- RPS/Models/RPS.cs
+++ RPS/Models/RPS.cs
@@ -23,22 +23,54 @@ namespace Game
             else if (userChoice == "p" && compChoice == "r" || userChoice == "r" && compChoice == "s" || userChoice == "s" && compChoice == "p")
             {
                 result = "User wins!";
+                User.Wins ++;
             }
             else if (compChoice == "p" && userChoice == "r" || compChoice == "r" && userChoice == "s" || compChoice == "s" && userChoice == "p")
             {
                 result = "Computer wins!";
+                Comp.Wins ++;
             }
             else
             {
                 result = "Try again.";
             }
+            if (CheckForTwoWins())
+            {
+                if (User.Wins == 2)
+                {
+                    result = "USER HAS WON THE MATCH!";
+                }
+                else if (Comp.Wins == 2)
+                {
+                    result = "COMPUTER HAS WON THE MATCH!";
+                }
+            }
             return result;
+        }
+
+        public bool CheckForTwoWins()
+        {
+            bool matchWinner = false;
+            if(User.Wins == 2)
+            {
+                matchWinner = true;
+            }     
+            else if(Comp.Wins == 2)
+            {
+                matchWinner = true;
+            }   
+            return matchWinner;
         }
     }
 
     public class Player
     {
         public string Item { get; set; }
+        public int Wins { get; set; }
+        public Player()
+        {
+            Wins = 0;
+        }
         public void ChooseItem(string item)
         {
             if (item[0].ToString().ToLower() == "r")
--- RPS/Program.cs
+++ RPS/Program.cs
@@ -5,30 +5,40 @@ class Program
 {
     static void Main()
     {
-        StartGame();
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        Console.WriteLine("Welcome to the classic game of Rock, Paper, Scissors!");
+        Console.WriteLine("Best two out of three wins!!");
+        Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+        RPS rps  = new RPS();
+        StartGame(rps);
     }
 
-    public static void StartGame()
+    public static void StartGame(RPS rps)
     {
         Console.WriteLine("Type rock, paper, or scissors (R/P/S):");
         string input = Console.ReadLine();
-        RPS rps  = new RPS();
         rps.User.ChooseItem(input);
+        rps.Comp.ChooseRandomly();
+        Console.WriteLine(rps.GamePlay());
+
         Console.WriteLine("------------------------------");
         Console.WriteLine("You chose " + rps.User.Item);
-        rps.Comp.ChooseRandomly();
         Console.WriteLine("Computer chose " + rps.Comp.Item);
-        Console.WriteLine(rps.GamePlay());
         Console.WriteLine("------------------------------");
-        Console.WriteLine("Play again? (Y/N)");
-        string response = Console.ReadLine();
-        if (response[0].ToString().ToUpper() == "Y")
+
+        if (rps.CheckForTwoWins())
         {
-            StartGame();
+            Console.WriteLine("Play again? (Y/N)");
+            string response = Console.ReadLine();
+            if (response[0].ToString().ToUpper() == "Y")
+            {
+                StartGame(rps);
+            }
+            else
+            {
+                Console.WriteLine("Bye!");
+            }
         }
-        else
-        {
-            Console.WriteLine("Bye!");
-        }
+        
     }
 }
